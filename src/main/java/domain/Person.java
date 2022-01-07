862722da66afbb0c45b7cbf29d129d5f6a6eba28
miao <2368264107@qq.com>
2022-01-07 13:27:39 +0800

    update

---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,13 @@
+package domain;
+
+/**
+ * @author Miaocunfu
+ * @Description
+ * @Classname Person
+ * @date 2022/1/7 13:26
+ */
+
+public class Person {
+    private String name;
+    private String address;
+}
